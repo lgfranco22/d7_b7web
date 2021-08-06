233ad533d6df582d74540ef3a49edd95bef63665
Luiz G F Michelmann <lgfranco22@live.com>
2021-08-06 17:57:19 -0300

    arquivo de configuração de credenciais

---
+++ p6/config.php
@@ -0,0 +1,13 @@
+<?php
+
+// credenciais do db
+define("DB_HOST", "localhost");
+define("DB_NAME", "quiz");
+define("DB_USER", "root");
+define("DB_PASS", "");
+
+// credenciais pagina de adm
+define("USER", "admin");
+define("PASS", "#FFFFFF1");
+
+?>
