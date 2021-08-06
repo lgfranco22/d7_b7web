69caaeeba7fc36de26533d2f78d15dec02ef6f69
Luiz G F Michelmann <lgfranco22@live.com>
2021-08-06 17:56:38 -0300

    update processamento

--- p6/processa.php
+++ p6/processa.php
@@ -1,15 +1,15 @@
 <?php
+//header("Content-Type: application/json");
+$options = array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
 
-if(isset($_POST)){
+$pdo = new PDO('mysql: host=localhost;dbname=quiz', 'root', '', $options);
+
+if(isset($_POST['nome'])){
 
 $nome = addslashes(htmlentities($_POST['nome']));
 $pontos = addslashes(htmlentities($_POST['pontos']));
 $ip = $_SERVER['REMOTE_ADDR'];
 $ua = $_SERVER['HTTP_USER_AGENT'];
-
-$options = array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
-
-$pdo = new PDO('mysql: host=localhost;dbname=quiz', 'root', '', $options);
 
 $stmt = $pdo->prepare("INSERT INTO participacao (ip, nome, user_agent, pontos) VALUES (:ip, :nm, :ua, :p)");
 $stmt->bindValue(':ip',$ip);
@@ -19,3 +19,21 @@ $stmt->bindValue(':p',$pontos);
 $stmt->execute();
 
 }
+
+function getData(){
+    $options = array(\PDO::ATTR_ERRMODE => \PDO::ERRMODE_EXCEPTION, \PDO::MYSQL_ATTR_INIT_COMMAND => "SET NAMES utf8");
+
+    $pdo = new PDO('mysql: host=localhost;dbname=quiz', 'root', '', $options);
+    
+    $stmt = $pdo->query("SELECT * FROM participacao");
+    $stmt = $stmt->fetchAll(PDO::FETCH_ASSOC);
+    echo json_encode($stmt);
+}
+
+getData();
+// if(isset($_GET['data'])){
+//     $stmt = $pdo->query("SELECT * FROM participacao");
+//     $stmt = $stmt->fetchAll(PDO::FETCH_ASSOC);
+//     echo json_encode($stmt);
+// }
+    
